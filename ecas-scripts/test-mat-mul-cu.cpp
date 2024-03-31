7836e4c418bbd144af118be405c36bb9d3febdb5
Java-Sith <prieto.luisdaniel@gmail.com>
2024-03-31 04:33:56 -0600

    Changed Cuda benchmark for C++

--- ecas-scripts/test-mat-mul-cu.cpp
+++ ecas-scripts/test-mat-mul-cu.cpp
@@ -1,23 +1,25 @@
-#include "ggml.h"
+extern "C" {
+    #include "ggml.h"
+    #if defined(GGML_GQ_USE_FP16_SCALE)
+    #define gq_scale_t ggml_fp16_t
+    #define GGML_FP32_TO_GQ(x) ggml_fp32_to_fp16(x)
+    #define GGML_GQ_TO_FP32(x) ggml_fp16_to_fp32(x)
+    #else
+    #define gq_scale_t float
+    #define GGML_FP32_TO_GQ(x) (x)
+    #define GGML_GQ_TO_FP32(x) (x)
+    #endif
+}
 
-#include <float.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <inttypes.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-
-#if defined(GGML_GQ_USE_FP16_SCALE)
-#define gq_scale_t ggml_fp16_t
-#define GGML_FP32_TO_GQ(x) ggml_fp32_to_fp16(x)
-#define GGML_GQ_TO_FP32(x) ggml_fp16_to_fp32(x)
-#else
-#define gq_scale_t float
-#define GGML_FP32_TO_GQ(x) (x)
-#define GGML_GQ_TO_FP32(x) (x)
-#endif
+#include <cfloat>
+#include <cstdint>
+#include <cstdio>
+#include <cinttypes>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
 
 #if defined(GGML_USE_HIPBLAS)
 #include <hip/hip_runtime.h>
@@ -78,80 +80,82 @@ const int K = 1280;
 #endif
 #endif
 
-//Naive implementation of Mul Mat
-void mul_mat(float* restrict src0, float* restrict src1, float *dst, int m, int n, int k) {
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            float sum = 0;
-            for (int l = 0; l < k; l++) {
-                sum += src0[i*k + l] * src1[j*k + l];
-            }
-            dst[i*n + j] = sum;
-        }
-    }
-}
+extern "C" {
+  //Naive implementation of Mul Mat
+  void mul_mat(float* restrict src0, float* restrict src1, float *dst, int m, int n, int k) {
+      for (int i = 0; i < m; i++) {
+          for (int j = 0; j < n; j++) {
+              float sum = 0;
+              for (int l = 0; l < k; l++) {
+                  sum += src0[i*k + l] * src1[j*k + l];
+              }
+              dst[i*n + j] = sum;
+          }
+      }
+  }
 
-static inline int quantize_4_blocks_per_row(int k) {
-    return k/QK;
-}
+  static inline int quantize_4_blocks_per_row(int k) {
+      return k/QK;
+  }
 
-static inline int quantize_4_row_size(int k) {
-    const int nb = quantize_4_blocks_per_row(k);
+  static inline int quantize_4_row_size(int k) {
+      const int nb = quantize_4_blocks_per_row(k);
 
-    return nb*(2*sizeof(gq_scale_t) + QK/2);
-}
+      return nb*(2*sizeof(gq_scale_t) + QK/2);
+  }
 
-void vec_dot_gq_4 (const int n, float * restrict s, const float * restrict x, const float * restrict y) {
-    const int nb = quantize_4_blocks_per_row(n);
+  void vec_dot_gq_4 (const int n, float * restrict s, const float * restrict x, const float * restrict y) {
+      const int nb = quantize_4_blocks_per_row(n);
 
-    const gq_scale_t * restrict pm0 = (const gq_scale_t *) x;
-    const gq_scale_t * restrict pm1 = (const gq_scale_t *) y;
+      const gq_scale_t * restrict pm0 = (const gq_scale_t *) x;
+      const gq_scale_t * restrict pm1 = (const gq_scale_t *) y;
 
-    const gq_scale_t * restrict pd0 = pm0 + nb;
-    const gq_scale_t * restrict pd1 = pm1 + nb;
+      const gq_scale_t * restrict pd0 = pm0 + nb;
+      const gq_scale_t * restrict pd1 = pm1 + nb;
 
-    const uint8_t * restrict pb0 = (const uint8_t *) (pd0 + nb);
-    const uint8_t * restrict pb1 = (const uint8_t *) (pd1 + nb);
+      const uint8_t * restrict pb0 = (const uint8_t *) (pd0 + nb);
+      const uint8_t * restrict pb1 = (const uint8_t *) (pd1 + nb);
 
-    float sumf = 0.0;
+      float sumf = 0.0;
 
-    for (int i = 0; i < nb; i++) {
-        const float m0 = GGML_GQ_TO_FP32(pm0[i]);
-        const float d0 = GGML_GQ_TO_FP32(pd0[i]);
+      for (int i = 0; i < nb; i++) {
+          const float m0 = GGML_GQ_TO_FP32(pm0[i]);
+          const float d0 = GGML_GQ_TO_FP32(pd0[i]);
 
-        const float m1 = GGML_GQ_TO_FP32(pm1[i]);
-        const float d1 = GGML_GQ_TO_FP32(pd1[i]);
+          const float m1 = GGML_GQ_TO_FP32(pm1[i]);
+          const float d1 = GGML_GQ_TO_FP32(pd1[i]);
 
-        const uint8_t * restrict p0 = pb0 + i*QK/2;
-        const uint8_t * restrict p1 = pb1 + i*QK/2;
+          const uint8_t * restrict p0 = pb0 + i*QK/2;
+          const uint8_t * restrict p1 = pb1 + i*QK/2;
 
-        for (int j = 0; j < QK/2; j++) {
-            const uint8_t v0 = p0[j];
-            const uint8_t v1 = p1[j];
+          for (int j = 0; j < QK/2; j++) {
+              const uint8_t v0 = p0[j];
+              const uint8_t v1 = p1[j];
 
-            const float f0 = d0*(v0 & 0xf) + m0;
-            const float f1 = d0*(v0 >> 4)  + m0;
+              const float f0 = d0*(v0 & 0xf) + m0;
+              const float f1 = d0*(v0 >> 4)  + m0;
 
-            const float f2 = d1*(v1 & 0xf) + m1;
-            const float f3 = d1*(v1 >> 4)  + m1;
+              const float f2 = d1*(v1 & 0xf) + m1;
+              const float f3 = d1*(v1 >> 4)  + m1;
 
-            sumf += f0*f2 + f1*f3;
-        }
-    }
-    *s = sumf;
-}
+              sumf += f0*f2 + f1*f3;
+          }
+      }
+      *s = sumf;
+  }
 
-void mul_mat_gq_4(const void * src0, const float * src1, float * dst, int m, int n, int k) {
-    assert(k % QK == 0);
-    for (int ir0 = 0; ir0 < m; ir0++) {
-        for (int ir1 = 0; ir1 < n; ir1++) {
-            vec_dot_gq_4(k, dst + ir1, src0, src1);
-            src1 = (const char *) src1 + quantize_4_row_size(k);
-        }
-        src0 = (const char *) src0 +   quantize_4_row_size(k);
-        src1 = (const char *) src1 - n*quantize_4_row_size(k);
-        dst = (float *) dst + n;
-    }
+  void mul_mat_gq_4(const void * src0, const float * src1, float * dst, int m, int n, int k) {
+      assert(k % QK == 0);
+      for (int ir0 = 0; ir0 < m; ir0++) {
+          for (int ir1 = 0; ir1 < n; ir1++) {
+              vec_dot_gq_4(k, dst + ir1, src0, src1);
+              src1 = (const char *) src1 + quantize_4_row_size(k);
+          }
+          src0 = (const char *) src0 +   quantize_4_row_size(k);
+          src1 = (const char *) src1 - n*quantize_4_row_size(k);
+          dst = (float *) dst + n;
+      }
+  }  
 }
 
 void printMatrix(float *matrix, int M, int N) {
