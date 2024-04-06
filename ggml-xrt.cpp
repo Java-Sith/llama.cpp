d216ea02e562770268cf7322fae6c2913e527a29
Java-Sith <prieto.luisdaniel@gmail.com>
2024-04-06 00:08:58 -0600

    Corrected a mistake in the class

--- ggml-xrt.cpp
+++ ggml-xrt.cpp
@@ -1,4 +1,5 @@
 #include "ggml-xrt.h"
+#include <vector>
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
--- ggml-xrt.h
+++ ggml-xrt.h
@@ -10,7 +10,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <vector>
 #include <iostream>
 
 #define QK4_0 32
