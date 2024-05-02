d0283f1adf4693c95d07163cda983de5a328fdea
Java-Sith <prieto.luisdaniel@gmail.com>
2024-05-02 03:17:28 -0600

    Checking where it crashes

--- ggml-xrt.cpp
+++ ggml-xrt.cpp
@@ -303,6 +303,7 @@ void ggml_xrt_mul_mat(
             if (type != GGML_TYPE_F32) {
                 x = (float *) params->wdata + i13*ne12*ne_plane + i12*ne_plane;
             }
+            printf("Checking if its here! 1");
 
             auto bo_a_mm = xrt::bo(myDevice, size_a * sizeof(uint32_t), matmul.group_id(0));
             auto bo_b_mm = xrt::bo(myDevice, size_b * sizeof(uint32_t), matmul.group_id(1));
@@ -311,6 +312,7 @@ void ggml_xrt_mul_mat(
             auto bo_b_mm_map = bo_b_mm.map<uint32_t*>();
             auto bo_c_mm_map = bo_c_mm.map<uint32_t*>();
             std::cout << "Filling Buffers\n";
+            printf("Checking if its here! 2");
 
             for (int elem = 0; elem < size_a; ++elem) {
                 //std::cout << as.V << " ";
