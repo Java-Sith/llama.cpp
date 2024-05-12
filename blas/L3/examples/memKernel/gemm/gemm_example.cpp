cb38442148f31c5d291f4720e4bb7feebacedd30
Java-Sith <prieto.luisdaniel@gmail.com>
2024-05-12 15:09:57 -0600

    Adding example of matmul

--- blas/L3/examples/memKernel/gemm/gemm_example.cpp
+++ blas/L3/examples/memKernel/gemm/gemm_example.cpp
@@ -71,132 +71,124 @@ bool compareGemm(BLAS_dataType* c, BLAS_dataType* goldenC, float p_TolRel = 1e-3
 }
 
 int main(int argc, char** argv) {
-    if (argc < 3) {
+    if (argc < 3){
         cerr << " usage: \n"
-             << " gemm_test.exe gemx.xclbin config_info.dat 1\n"
-             << " gemm_test.exe gemx.xclbin config_info.dat\n";
+            << " gemm_common_test.exe gemx.xclbin config_info.dat 1\n"
+            << " gemm_common_test.exe gemx.xclbin config_info.dat\n";
         return EXIT_FAILURE;
     }
     unsigned int l_argIdx = 1;
     string l_xclbinFile(argv[l_argIdx++]);
     string l_configFile(argv[l_argIdx++]);
+
     int l_numKernel = 1;
 
-    if (argc == 4) {
-        cout << "read custom number of kernels\n";
+    if (argc == 4){
+        cout<<"read custom number of kernels\n";
         l_numKernel = stoi(argv[l_argIdx++]);
     }
 
+    int i, j; // i-row index ,j- column index
+
+    BLAS_dataType * a, * b, * c;
+    a = ( BLAS_dataType *) malloc (m*k* sizeof ( BLAS_dataType )); // host memory for a
+    b = ( BLAS_dataType *) malloc (k*n* sizeof ( BLAS_dataType ));
+    c = ( BLAS_dataType *) malloc (m*n* sizeof ( BLAS_dataType ));
+
+    int ind = 1;
+
+    for( i = 0; i<  m; i ++){
+        for( j = 0; j < k; j ++){
+        a[ IDX2R (i,j,k )]=( BLAS_dataType ) ind++;
+        }
+    }
+
+    for( i = 0; i<  k; i ++){
+        for( j = 0; j < n; j ++){
+        b[ IDX2R (i,j,n )]=( BLAS_dataType ) ind++;
+        }
+    }
+
+    for( i = 0; i<  m; i ++){
+        for( j = 0; j < n; j ++){
+        c[ IDX2R (i,j,n )]= 0;
+        }
+    }
+
+    BLAS_dataType * d_a, * d_b, * d_c;
+
+
     xfblasEngine_t engineName = XFBLAS_ENGINE_GEMM;
-    xfblasStatus_t status = xfblasCreate(l_xclbinFile.c_str(), l_configFile, engineName, l_numKernel);
+    xfblasStatus_t status = XFBLAS_STATUS_SUCCESS;
+
+    status = xfblasCreate(l_xclbinFile.c_str(), l_configFile, engineName, l_numKernel);
     if (status != XFBLAS_STATUS_SUCCESS) {
-        cout << "Create Handle failed with error code: " << status << "\n";
+        cout<<"Create Handle failed with error code: "<< status << "\n";
         return EXIT_FAILURE;
     }
 
-    int i, j; // i-row l_numKernel -1 ,j- column l_numKernel -1
-    BLAS_dataType *a, *b, *c;
-
-    posix_memalign((void**)&a, 4096, m * k * sizeof(BLAS_dataType));
-    posix_memalign((void**)&b, 4096, k * n * sizeof(BLAS_dataType));
-    posix_memalign((void**)&c, 4096, m * n * sizeof(BLAS_dataType));
-
-    int ind = 1;
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < k; j++) {
-            a[IDX2R(i, j, k)] = (BLAS_dataType)ind++;
-        }
-    }
-    ind = 1;
-    for (i = 0; i < k; i++) {
-        for (j = 0; j < n; j++) {
-            b[IDX2R(i, j, n)] = (BLAS_dataType)ind++;
-        }
-    }
-
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++) {
-            c[IDX2R(i, j, n)] = 0;
-        }
-    }
-
-    BLAS_dataType* goldenC = getGoldenMat(a, b, c);
-
-    BLAS_dataType *d_a, *d_b, *d_c;
-
-    status = xfblasMalloc(&d_a, m, k, sizeof(*a), l_numKernel-1);
+    status = xfblasMalloc(&d_a, m,k,sizeof(*a), l_numKernel-1);
 
     if (status != XFBLAS_STATUS_SUCCESS) {
         cout<<"Malloc memory for matrix A failed with error code: "<< status << "\n";
         return EXIT_FAILURE;
     }
-    status = xfblasMalloc(&d_b, k, n, sizeof(*b), l_numKernel-1);
+    status = xfblasMalloc(&d_b, k,n,sizeof(*b), l_numKernel-1);
 
     if (status != XFBLAS_STATUS_SUCCESS) {
         cout<<"Malloc memory for matrix B failed with error code: "<< status << "\n";
         return EXIT_FAILURE;
     }
 
-    status = xfblasMalloc(&d_c, m, n, sizeof(*c), l_numKernel-1);
+    status = xfblasMalloc(&d_c, m,n,sizeof(*c), l_numKernel-1);
 
     if (status != XFBLAS_STATUS_SUCCESS) {
         cout<<"Malloc memory for matrix C failed with error code: "<< status << "\n";
         return EXIT_FAILURE;
     }
 
-    status = xfblasSetMatrix(m, k, sizeof(*a), a, k, d_a, l_numKernel-1);
-    status = xfblasSetMatrix(k, n, sizeof(*b), b, n, d_b, l_numKernel-1);
-    status = xfblasSetMatrix(m, n, sizeof(*c), c, n, d_c, l_numKernel-1);
+    status = xfblasSetMatrix(m,k,sizeof(*a),a,k,d_a, l_numKernel-1);
+    status = xfblasSetMatrix(k,n,sizeof(*b),b,n,d_b, l_numKernel-1);
+    status = xfblasSetMatrix(m,n,sizeof(*c),c,n,d_c, l_numKernel-1);
 
     if (status != XFBLAS_STATUS_SUCCESS) {
         cout<<"Set Matrix failed with error code: "<< status << "\n";
         return EXIT_FAILURE;
     }
 
-    // Start the clock
-    auto start_time = std::chrono::high_resolution_clock::now();
-
-    status = xfblasGemm(XFBLAS_OP_N, XFBLAS_OP_N, m, n, k, 1, a, k, b, n, 1, c, n, l_numKernel - 1);
-
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto matmul_time = end_time - start_time;
+    status = xfblasGemm(XFBLAS_OP_N, XFBLAS_OP_N, m, n, k, 1, d_a, k, d_b, n, 1, d_c, n, l_numKernel-1);
 
     if (status != XFBLAS_STATUS_SUCCESS) {
         cout<<"Matrix Multiplication failed with error code: "<< status << "\n";
         return EXIT_FAILURE;
     }
-    
     status = xfblasGetMatrix(m,n,sizeof(*c),d_c,c,n, l_numKernel-1);
 
     if (status != XFBLAS_STATUS_SUCCESS) {
-        cout<<"Get Matirx failed with error code: "<< status << "\n";
+        cout<<"Get Matrix failed with error code: "<< status << "\n";
         return EXIT_FAILURE;
     }
 
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            cout << (c[IDX2R(i, j, k)]) << " ";
+    for ( i = 0; i < m; i ++){
+        for ( j = 0; j < n; j ++){
+        cout<< (c[ IDX2R (i,j, k )])<<" ";
         }
-        cout << "\n";
+        cout<<"\n";
     }
 
-    if (compareGemm(c, goldenC)) {
-        cout << "Test passed!\n";
-    } else {
-        cout << "Test failed!\n";
-    }
+    // 590 605 620 635 650
+    // 1490 1530 1570 1610 1650
+    // 2390 2455 2520 2585 2650
+    // 3290 3380 3470 3560 3650
+    // 4190 4305 4420 4535 4650
 
-    std::cout << "Matrix multiplication = " << matmul_time/std::chrono::milliseconds(1) << " ms " << '\n';
-
-    xfblasFree(a, l_numKernel - 1);
-    xfblasFree(b, l_numKernel - 1);
-    xfblasFree(c, l_numKernel - 1);
+    xfblasFree(d_a, l_numKernel-1);
+    xfblasFree(d_b, l_numKernel-1);
+    xfblasFree(d_c, l_numKernel-1);
+    xfblasDestroy(l_numKernel);
     free(a);
     free(b);
     free(c);
-    free(goldenC);
-
-    xfblasDestroy(l_numKernel);
 
     return EXIT_SUCCESS;
 }
