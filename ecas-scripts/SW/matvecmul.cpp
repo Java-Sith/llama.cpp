10ee4020be8c8a4edd83511b1f1ccbbdc60c0e68
Java-Sith <prieto.luisdaniel@gmail.com>
2024-08-04 15:26:26 -0600

    Changed testbench for GEMV
    Testbench takes into account curiosities of the code I wasn't taking into account before

--- ecas-scripts/SW/matvecmul.cpp
+++ ecas-scripts/SW/matvecmul.cpp
@@ -1,3 +1,19 @@
+/**
+* Copyright (C) 2019-2021 Xilinx, Inc
+*
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
+
 #include <iostream>
 #include <cstdint>
 #include <cstring>
@@ -13,129 +29,134 @@
 // HLS Types
 #include "ap_fixed.h"
 
-// Function to find the next power of two greater than or equal to n
-int next_power_of_two(int n) {
-    if (n <= 64) {
-    	return 64;
-    } else {
-    	return pow(2, ceil(log2(n)));
-    }
-}
-
 int main(int argc, char** argv) {
-    INIT_PROFILER(cynq_profiler)
-    int device_index = 0;
+  INIT_PROFILER(cynq_profiler)
+  int device_index = 0;
 
-    if (argc != 4) {
-        return EXIT_FAILURE;
+  if (argc != 3) {
+      return EXIT_FAILURE;
+  }
+  
+  // Get input size
+  static std::string binaryFile = "../HW/package.hw/kernels.xclbin";
+  int a_rows = std::stoi(argv[1]);
+  int b_cols = std::stoi(argv[2]);
+  //b_cols = b_cols < 8 ? 8 : (b_cols - (b_cols & 0b111));
+  int c_cols = 1;
+
+  std::cout << "A rows: " << a_rows << "\n"
+            << "B cols: " << b_cols << "\n"
+            << "C cols: " << c_cols << std::endl;
+
+  // Compute sizes
+  int size_a = a_rows * b_cols;
+  int size_b = c_cols * b_cols;
+  int size_c = a_rows * c_cols;
+
+  int padded_size_a = next_power_of_two(size_a);
+  int padded_size_b = next_power_of_two(size_b);
+  int padded_size_c = next_power_of_two(size_c);
+
+  GET_PROFILE_INSTANCE(setup_time, cynq_profiler);
+  setup_time->reset();
+
+  std::cout << "Open the device" << device_index << std::endl;
+  auto device = xrt::device(device_index);
+  std::cout << "Load the xclbin " << binaryFile << std::endl;
+  auto uuid = device.load_xclbin(binaryFile);;
+
+  auto matvecmul = xrt::kernel(device, uuid, "matvecmul");
+  setup_time->tick();
+
+  std::cout << "Allocate Buffer in Global Memory\n";
+  auto bo_a = xrt::bo(device, padded_size_a * sizeof(float), matvecmul.group_id(0));
+  auto bo_b = xrt::bo(device, padded_size_b * sizeof(float), matvecmul.group_id(1));
+  auto bo_c = xrt::bo(device, padded_size_c * sizeof(float), matvecmul.group_id(2));
+
+  // Map the contents of the buffer object into host memory
+  auto bo_a_map = bo_a.map<float*>();
+  auto bo_b_map = bo_b.map<float*>();
+  auto bo_c_map = bo_c.map<float*>();
+
+  std::fill(bo_a_map, bo_a_map + padded_size_a, 0.0f);
+  std::fill(bo_b_map, bo_b_map + padded_size_b, 0.0f);
+  std::fill(bo_c_map, bo_c_map + padded_size_c, 0.0f);
+  
+  // Filling data
+  std::cout << "Filling Buffers\n";
+  // Fill A
+  float as = 0.3f;
+  std::cout << "A: " << std::endl;
+  for (int row = 0; row < a_rows; ++row) {
+      for (int col = 0; col < b_cols; ++col) {
+      bo_a_map[col + row * b_cols] = as;
+      if (a_rows < 16 && b_cols < 16) {
+        std::cout << as << ", ";
+      }
+      as += 0.01;
+      }
+      if (a_rows < 16 && b_cols < 16) {
+        std::cout << std::endl;
+      }
+  }
+
+  // Fill B
+  float bs = 1.0f;
+  std::cout << "B: " << std::endl;
+  for (int col = 0; col < b_cols; ++col) {
+      bo_b_map[col] = bs;
+      if (a_rows < 16 && b_cols < 16) {
+        std::cout << bs << ", ";
+      }
+      bs += 0.05;
+  }
+  if (a_rows < 16 && b_cols < 16) {
+    std::cout << std::endl;
+  }
+
+  // Synchronize buffer content with device side
+  std::cout << "synchronize input buffer data to device global memory\n";
+  START_PROFILE(kernel_execution, cynq_profiler, 10)
+  bo_a.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_b.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+
+  //std::cout << "Execution of the kernel\n";
+  auto run = matvecmul(bo_a, bo_b, bo_c, a_rows, b_cols, c_cols);
+  //std::cout << "Waiting to the end\n";
+  run.wait();
+
+  // Get the output;
+  //std::cout << "Get the output data from the device" << std::endl;
+  bo_c.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+  END_PROFILE(kernel_execution);
+
+  // Multiply by software
+  float c_sw[padded_size_c];
+  std::fill(c_sw, c_sw + padded_size_c, 0.0f);
+  for (int row = 0; row < a_rows; ++row) {
+      c_sw[row] = 0.f;
+      for (int k = 0; k < b_cols; ++k) {
+        c_sw[row] += bo_a_map[row * b_cols + k] * bo_b_map[k];
+      }
+  }
+
+  // Compare results
+  std::cout << "C_SW - C_HW: " << std::endl;
+  for (int row = 0; row < a_rows; ++row) {
+    if (a_rows < 16 && b_cols < 16) {
+      std::cout << c_sw[row] << " - " << bo_c_map[row] << std::endl;
     }
-
-    // Get input size
-    static std::string binaryFile = "../HW/package.hw/kernels.xclbin";
-    int a_rows = std::stoi(argv[1]);
-    int b_cols = std::stoi(argv[2]);
-    //b_cols = b_cols < 8 ? 8 : (b_cols - (b_cols & 4));
-    int c_cols = std::stoi(argv[3]);
-    //c_cols = c_cols < 8 ? 8 : (c_cols - (c_cols & 4));
-    //int b_rows = std::stoi(argv[4]);
-
-    std::cout << "A rows: " << a_rows << "\n"
-              << "B cols: " << b_cols << "\n"
-              << "C cols: " << c_cols << std::endl;
-
-    // Compute sizes
-    int size_a = a_rows * b_cols;
-    int size_b = 1 * b_cols;
-    int size_c = 1 * c_cols;
-
-    int padded_size_a = next_power_of_two(size_a);
-    int padded_size_b = next_power_of_two(size_b);
-    int padded_size_c = next_power_of_two(size_c);
-
-    GET_PROFILE_INSTANCE(setup_time, cynq_profiler);
-    setup_time->reset();
-
-    std::cout << "Open the device " << device_index << std::endl;
-    auto device = xrt::device(device_index);
-    std::cout << "Load the xclbin " << binaryFile << std::endl;
-    auto uuid = device.load_xclbin(binaryFile);
-    auto matvecmul = xrt::kernel(device, uuid, "matvecmul");
-    setup_time->tick();
-
-    std::cout << "Allocate Buffer in Global Memory\n";
-    auto bo_a = xrt::bo(device, padded_size_a * sizeof(float), matvecmul.group_id(0));
-    auto bo_b = xrt::bo(device, padded_size_b * sizeof(float), matvecmul.group_id(1));
-    auto bo_c = xrt::bo(device, padded_size_c * sizeof(float), matvecmul.group_id(2));
-
-    // Map the contents of the buffer object into host memory
-    auto bo_a_map = bo_a.map<float*>();
-    auto bo_b_map = bo_b.map<float*>();
-    auto bo_c_map = bo_c.map<float*>();
-
-    // Filling data
-    std::cout << "Filling Buffers\n";
-    //std::copy(a.begin(), a.end(), bo_a_mm_map);
-    //std::copy(b.begin(), b.end(), bo_b_mm_map);
-
-    float as = 0.02, bs = 0.03;
-    std::cout << "A: " << std::endl;
-    for (int elem = 0; elem < size_a; ++elem) {
-        //std::cout << as.V << " ";
-        bo_a_map[elem] = as;
-        //std::cout << std::hex << as.V << " ";
-        as += 0.03;
-        if ((elem + 1) % b_cols == 0) {
-            //std::cout << std::endl;
-            as = 0.025;
-        }
+    float err = fabs(c_sw[row] - bo_c_map[row]) / c_sw[row];
+    // Check for 0.1%
+    if (err > 0.001) {
+      std::cerr << "Error in index: " << row
+                << " Val: " << err
+                << " C_HW: " << bo_c_map[row] << " C_SW: " << c_sw[row] << std::endl;
+      return -1;
     }
+  }
 
-    //for (int row = 0; row < b_rows; ++row)
-    //{
-    std::cout << "B: " << std::endl;
-    for (int elem = 0; elem < size_b; ++elem) {
-        //std::cout << bs.V << " ";
-        //std::cout << std::hex << bs.V << " ";
-        bo_b_map[elem] = bs;
-        bs += 0.07;
-        if ((elem + 1) % b_cols == 0) {
-            //std::cout << std::endl;
-            bs = 0.04;
-        }
-    }
-    // std::cout << std::endl;
-    std::fill(bo_c_map, bo_c_map + padded_size_c, 0.0f);
-
-    // Synchronize buffer content with device side
-    std::cout << "Synchronize input buffer data to device global memory\n";
-    START_PROFILE(kernel_execution, cynq_profiler, 1)
-    bo_a.sync(XCL_BO_SYNC_BO_TO_DEVICE);
-    bo_b.sync(XCL_BO_SYNC_BO_TO_DEVICE);
-
-    //matvecmul(a, b + row * COLS_B, c + row * ROWS_A, ROWS_A, COLS_A, 1);
-    std::cout << "Execution of the kernel: matvecmul\n";
-    auto run = matvecmul(bo_a, bo_b, bo_c, a_rows, b_cols, c_cols);
-    std::cout << "Waiting to the end\n";
-    run.wait();
-
-    // Get the output;
-    std::cout << "Get the output data from the device" << std::endl;
-    bo_c.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
-    END_PROFILE(kernel_execution);
-
-    std::cout << "C: " << std::endl;
-    for (int elem = 0; elem < size_c; ++elem) {
-        float cs;
-        cs = bo_c_map[elem];
-        //std::cout << cs << " ";
-        //std::cout << std::hex << cs.V << " ";
-        //if ((elem + 1) % c_cols == 0) std::cout << std::endl;
-    }
-    // std::cout << std::endl;
-    // Print the duration
-    std::cout << cynq_profiler << std::endl;
-    //}
-
-    std::cout << "TEST PASSED\n";
-    return 0;
+  std::cout << cynq_profiler << std::endl;
+  std::cout << "TEST PASSED\n";
+  return 0;
 }
