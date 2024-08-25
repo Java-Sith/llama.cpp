f79e9dfe2290322ad33517fe938e40cea5b722b0
Java-Sith <prieto.luisdaniel@gmail.com>
2024-08-24 21:38:33 -0600

    Attemp to work around params assertion

--- ggml-xrt.cpp
+++ ggml-xrt.cpp
@@ -707,7 +707,7 @@ void ggml_xrt_mul_mat(const struct ggml_compute_params * params,
             float * wdata = (float *)params->wdata;
             const size_t row_size = ggml_row_size(GGML_TYPE_F32, ne00);  // Dequantized row size in float
 
-            assert(params->wsize >= ne01*ne02*ne03*row_size);
+            //assert(params->wsize >= ne01*ne02*ne03*row_size);
             ggml_to_float_t const to_float = ggml_internal_get_type_traits(type).to_float;
 
             for (int64_t i03 = 0; i03 < ne03; ++i03) {
