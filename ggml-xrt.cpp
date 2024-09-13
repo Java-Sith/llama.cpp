813df60db3395b56d14b543c14dfc0174915b55a
Java-Sith <prieto.luisdaniel@gmail.com>
2024-09-13 03:21:07 -0600

    Is Softmax a team player

--- ggml-xrt.cpp
+++ ggml-xrt.cpp
@@ -211,7 +211,7 @@ static void ggml_xrt_add(
     struct ggml_tensor *dst)
 {
 
-    const struct ggml_tensor * src0 = dst->src[0];
+    /*const struct ggml_tensor * src0 = dst->src[0];
     const struct ggml_tensor * src1 = dst->src[1];
 
     GGML_ASSERT(src1->type == GGML_TYPE_F32);
@@ -225,8 +225,8 @@ static void ggml_xrt_add(
             {
                 ggml_compute_forward_add(params, dst);
             } break;
-    }
-    //ggml_compute_forward_add(params, dst);
+    }*/
+    ggml_compute_forward_add(params, dst);
 }
 
 // ggml_compute_forward_mul
@@ -322,7 +322,7 @@ static void ggml_xrt_mul(
         const struct ggml_compute_params * params,
         struct ggml_tensor * dst) {
 
-    const struct ggml_tensor * src0 = dst->src[0];
+    /*const struct ggml_tensor * src0 = dst->src[0];
     const struct ggml_tensor * src1 = dst->src[1];
 
     GGML_ASSERT(src1->type == GGML_TYPE_F32);
@@ -336,8 +336,8 @@ static void ggml_xrt_mul(
             {
                 ggml_compute_forward_mul(params, dst);
             } break;
-    }
-    //ggml_compute_forward_mul(params, dst);
+    }*/
+    ggml_compute_forward_mul(params, dst);
 }
 
 // ggml_compute_forward_transpose
@@ -455,7 +455,7 @@ static void ggml_xrt_rms_norm(
         const struct ggml_compute_params * params,
         struct ggml_tensor * dst) {
 
-    const struct ggml_tensor * src0 = dst->src[0];
+    /*const struct ggml_tensor * src0 = dst->src[0];
 
     GGML_ASSERT(src0->type == GGML_TYPE_F32);
 
@@ -468,8 +468,8 @@ static void ggml_xrt_rms_norm(
             {
                 ggml_compute_forward_rms_norm(params, dst);
             } break;
-    }
-    //ggml_compute_forward_rms_norm(params, dst);
+    }*/
+    ggml_compute_forward_rms_norm(params, dst);
 }
 
 static void ggml_xrt_rope(
